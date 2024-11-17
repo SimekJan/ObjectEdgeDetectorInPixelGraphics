e29f96d96238bc1cf962685cbdfe20dee2be8d77
simekja7 <jan.simek.4242@gmail.com>
2024-11-17 23:11:00 +0100

    Add project structure

---
+++ ObjectEdgeDetectorInPixelGraphics/src/main/java/cz/mff/cuni/simekja7/objectedgedetectorinpixelgraphics/ObjectEdgeDetectorInPixelGraphics.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cz.mff.cuni.simekja7.objectedgedetectorinpixelgraphics;
+
+/**
+ *
+ * @author simek.jan
+ */
+public class ObjectEdgeDetectorInPixelGraphics {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
