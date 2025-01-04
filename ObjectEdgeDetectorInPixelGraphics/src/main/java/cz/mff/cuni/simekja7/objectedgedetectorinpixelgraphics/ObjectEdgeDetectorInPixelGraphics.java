28073280b3d6044ebddcd2f266f02cfd584badf0
simekja7 <jan.simek.4242@gmail.com>
2025-01-04 17:12:47 +0100

    Add SIFT keypoints example

--- ObjectEdgeDetectorInPixelGraphics/src/main/java/cz/mff/cuni/simekja7/objectedgedetectorinpixelgraphics/ObjectEdgeDetectorInPixelGraphics.java
+++ ObjectEdgeDetectorInPixelGraphics/src/main/java/cz/mff/cuni/simekja7/objectedgedetectorinpixelgraphics/ObjectEdgeDetectorInPixelGraphics.java
@@ -6,6 +6,9 @@ package cz.mff.cuni.simekja7.objectedgedetectorinpixelgraphics;
 
 import org.opencv.core.Mat;
 import org.opencv.core.Core;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.features2d.Features2d;
+import org.opencv.features2d.SIFT;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
@@ -18,14 +21,27 @@ public class ObjectEdgeDetectorInPixelGraphics {
     static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
     
     public static void main(String[] args) {        
-        CannyEdgeExample(args);
+        CannyEdgeExample();
+        SIFTKeypointsExample();
     }
     
-    public static void CannyEdgeExample(String[] args) {
+    public static void CannyEdgeExample() {
         Mat src = Imgcodecs.imread("./london.jpg", Imgcodecs.IMREAD_GRAYSCALE);
         Mat edges = new Mat();
-        Imgproc.Canny(src, edges, 200, 400);
-        Imgcodecs.imwrite("./edges.jpg", edges);
+        Imgproc.Canny(src, edges, 100, 200);
+        Imgcodecs.imwrite("./edges_canny.jpg", edges);
+    }
+    
+    public static void SIFTKeypointsExample() {
+        Mat image = Imgcodecs.imread("./london.jpg", Imgcodecs.IMREAD_GRAYSCALE);
+        Mat edges = new Mat();
+        SIFT sift = SIFT.create();
+        Imgproc.Canny(image, edges, 100, 200);
+        MatOfKeyPoint keypoints = new MatOfKeyPoint();
+        sift.detect(edges, keypoints);
+        Mat outputImage = new Mat();
+        Features2d.drawKeypoints(image, keypoints, outputImage);
+        Imgcodecs.imwrite("./sift_keypoints.jpg", outputImage);
     }
 }
 
