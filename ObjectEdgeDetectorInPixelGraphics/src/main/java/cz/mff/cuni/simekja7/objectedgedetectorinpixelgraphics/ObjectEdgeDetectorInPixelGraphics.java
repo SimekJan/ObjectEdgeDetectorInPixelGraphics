69dcd3306404f8137efcd96c620603a6dfae7a9f
simekja7 <jan.simek.4242@gmail.com>
2025-01-04 16:27:05 +0100

    Add OpenCV providing library methods. Also add default implementation of Canny Edge from OpenCV.

--- ObjectEdgeDetectorInPixelGraphics/src/main/java/cz/mff/cuni/simekja7/objectedgedetectorinpixelgraphics/ObjectEdgeDetectorInPixelGraphics.java
+++ ObjectEdgeDetectorInPixelGraphics/src/main/java/cz/mff/cuni/simekja7/objectedgedetectorinpixelgraphics/ObjectEdgeDetectorInPixelGraphics.java
@@ -4,13 +4,28 @@
 
 package cz.mff.cuni.simekja7.objectedgedetectorinpixelgraphics;
 
+import org.opencv.core.Mat;
+import org.opencv.core.Core;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
 /**
  *
  * @author simek.jan
  */
 public class ObjectEdgeDetectorInPixelGraphics {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    
+    static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+    
+    public static void main(String[] args) {        
+        CannyEdgeExample(args);
+    }
+    
+    public static void CannyEdgeExample(String[] args) {
+        Mat src = Imgcodecs.imread("./london.jpg", Imgcodecs.IMREAD_GRAYSCALE);
+        Mat edges = new Mat();
+        Imgproc.Canny(src, edges, 200, 400);
+        Imgcodecs.imwrite("./edges.jpg", edges);
     }
 }
+
